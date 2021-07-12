be54df7d563c2daf6f1496ecb99236af1b2f50a5
oluwanifemi olayinka <olayinkaoluwanifemia@gmail.com>
2021-07-11 13:38:12 -1200

    bootstrap-react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import './App.css';
-import FCTToken from './FCTToken.sol'
+import Faucet from './components/Faucet.js'
+import FCTToken from './artifacts/contracts/FCTToken.sol/FCTToken.json'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Row, Col } from 'react-bootstrap'
 
 function App() {
 
@@ -7,9 +9,18 @@ function App() {
 
   return (
     <div className="App">
+    <Container>
+    <Row className="justify-content-md-center">
+      <Col>
+      <div>our faucet</div>
+      </Col>
+      <Col>
+      <div> our send area</div>
+      </Col>
+    </Row>
+    </Container>
     </div>
   );
 }
 
 export default App;
-
